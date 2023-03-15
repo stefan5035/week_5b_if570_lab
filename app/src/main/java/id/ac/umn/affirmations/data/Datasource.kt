96e9e6cfc5aa6bcf4c60d39cf4ed01e91e774ba5
stefan5035 <stefan.madethen@gmail.com>
2023-03-15 23:36:07 +0700

    RecyclerView

---
+++ app/src/main/java/id/ac/umn/affirmations/data/Datasource.kt
@@ -0,0 +1,20 @@
+package id.ac.umn.affirmations.data
+import id.ac.umn.affirmations.R
+import id.ac.umn.affirmations.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/id/ac/umn/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package id.ac.umn.affirmations.model
+
+data class Affirmation(val stringResourceId: Int){
+}
